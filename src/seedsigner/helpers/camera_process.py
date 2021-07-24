d81fe9ebe7a9834e3414264a6c8edcc7c40f31bb
kdmukai <keith.mukai@essaytagger.com>
2021-07-23 22:43:04 -0500

    search and replace bugfix

--- src/seedsigner/helpers/camera_process.py
+++ src/seedsigner/helpers/camera_process.py
@@ -147,20 +147,20 @@ class CameraPoll(object):
         self.function   = function
         self.args       = args
         self.kwargs     = kwargs
-        self.is_is_running = False
+        self.is_running = False
         self.start()
 
     def _run(self):
-        self.is_is_running = False
+        self.is_running = False
         self.start()
         self.function(*self.args, **self.kwargs)
 
     def start(self):
-        if not self.is_is_running:
+        if not self.is_running:
             self._timer = Timer(self.interval, self._run)
             self._timer.start()
-            self.is_is_running = True
+            self.is_running = True
 
     def stop(self):
         self._timer.cancel()
-        self.is_is_running = False
+        self.is_running = False
