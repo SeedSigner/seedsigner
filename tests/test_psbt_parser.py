f6bcdcf1ac1a6312af3f2d6bb27fe01296357bbb
newtonick <nick@klockenga.net>
2021-09-07 18:52:18 +0000

    add psbt_parser test

--- tests/test_psbt_parser.py
+++ tests/test_psbt_parser.py
@@ -1,6 +1,6 @@
 import pytest
 from mock import MagicMock
-from seedsigner.models.psbt_parser import PSBTParser
+from seedsigner.models import PSBTParser
 from embit import psbt
 
 def test_sig_count():
