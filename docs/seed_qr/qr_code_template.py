5cdce368fa4a20b10e79fe59679a9bcf61435293
kdmukai <kdmukai@gmail.com>
2022-01-23 23:21:24 -0600

    Updated printable templates; light docs/ dir reorg

--- docs/seed_qr/qr_code_template.py
+++ docs/seed_qr/qr_code_template.py
@@ -1,11 +1,9 @@
 import math
 
-def generate_qr_template(qr_size, block_size=5, show_timing_marks=False):
-    if qr_size == 29:
-        num_words = 24
+def generate_qr_template(qr_size, num_words=24, block_size=5, show_timing_marks=False):
+    if num_words == 24:
         word_cols = 2
     else:
-        num_words = 12
         word_cols = 1
 
     html = """
@@ -29,12 +27,10 @@ def generate_qr_template(qr_size, block_size=5, show_timing_marks=False):
             table {
                 margin-top: 1em;
                 border-collapse: collapse;
-                border: 1px solid black;
             }
             td {
                 padding:  0;
                 margin:  0;
-                # border: 1px dotted #c8c8c8;
                 width: 12px;
                 height: 12px;
             }
@@ -42,17 +38,17 @@ def generate_qr_template(qr_size, block_size=5, show_timing_marks=False):
                 text-align: center;
                 font-size: 0.35em;
                 color: black;
-                width: 9px;
+                width: 13px;
                 height: 9px;
             }
             .qr_table {
-                margin-top: 5em;
-                margin-right: 4em;
-                float: right;
+                margin-left: 4em;
+                display: inline-block;
             }
             .filled {
                 background-color: black;
-                border: 1px solid black;
+                border: none;
+                color: #ccc;
             }
             .col_name, .row_name {
                 text-align: center;
@@ -60,12 +56,18 @@ def generate_qr_template(qr_size, block_size=5, show_timing_marks=False):
                 color: #aaa;
             }
             .col_name {
+                border-top: 1px solid #bbb;
                 border-bottom: 1px solid #bbb;
                 height: 2em;
             }
             .row_name {
                 border-right: 1px solid #bbb;
+                border-left: 1px solid #bbb;
                 width: 2em;
+            }
+            .first_td {
+                border-top: 1px solid #bbb;
+                border-left: 1px solid #bbb;
             }
             .col_block_divider {
                 border-right: 1px solid #bbb;
@@ -74,10 +76,11 @@ def generate_qr_template(qr_size, block_size=5, show_timing_marks=False):
                 border-bottom: 1px solid #bbb;
             }
             .no_border {
-                border: none;
+                border: none !important;
             }
 
             .word_list_table {
+                border: 1px solid #aaa;
     """
 
     if num_words == 12:
@@ -130,7 +133,7 @@ def generate_qr_template(qr_size, block_size=5, show_timing_marks=False):
     y_names = "A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,X,Y,Z"
 
     html += """<table align="center" class="qr_table">"""
-    html += f"""<tr rowspan="{block_size}"><td class="col_block_divider row_block_divider"></td>"""
+    html += f"""<tr rowspan="{block_size}"><td class="col_block_divider row_block_divider first_td"></td>"""
     for j in range(0, math.ceil(qr_size/block_size)):
         html += f"""<td colspan="{block_size}" class="col_name col_block_divider">{j + 1}</td>"""
     html += "</tr>"
@@ -140,35 +143,44 @@ def generate_qr_template(qr_size, block_size=5, show_timing_marks=False):
         if i % block_size == 0:
             html += f"""<td rowspan="{block_size}" class="row_name row_block_divider qrcell">{y_names.split(",")[int(i / block_size)]}</td>"""
         for j in range(0, qr_size):
-            html += f"""<td class="qrcell" id="{i}_{j}">&middot;</td>"""
+            html += f"""<td class="qrcell {"row_block_divider" if i == qr_size - 1 else ""} {"col_block_divider" if j == qr_size -1 else ""}" id="{i}_{j}">&middot;</td>"""
         html += """</tr>\n"""
     html += "</table>"
 
     html += "<script>"
 
-    def fill(i, j):
-        return f"""document.getElementById("{i}_{j}").classList.add("filled");\n"""
+    def fill(i, j, clear_contents=True):
+        js_result = f"""document.getElementById("{i}_{j}").classList.add("filled");\n"""
+        if clear_contents:
+            js_result += f"""document.getElementById("{i}_{j}").textContent = '';\n"""
+        return js_result
 
     def no_border(i, j):
-        return f"""document.getElementById("{i}_{j}").classList.add("no_border");\n"""
+        js_result = f"""document.getElementById("{i}_{j}").classList.remove("row_block_divider");\n"""
+        js_result += f"""document.getElementById("{i}_{j}").classList.remove("col_block_divider");\n"""
+        js_result += f"""document.getElementById("{i}_{j}").classList.add("no_border");\n"""
+        return js_result
 
 
     # Generate the corner registration box
     def fill_registration_box(offset_i, offset_j):
         result = ""
         for i in range(0, 7):
+            clear_middot = i != 0 and i != 6
             i += offset_i
-            result += fill(i, offset_j + 0)
-            result += fill(i, offset_j + 6)
+            result += fill(i, offset_j + 0, clear_contents=clear_middot)
+            result += fill(i, offset_j + 6, clear_contents=clear_middot)
         for j in range(1, 6):
             j += offset_j
             result += fill (offset_i + 0, j)
             result += fill (offset_i + 6, j)
         for i in range(2, 5):
+            clear_middot = i != 2 and i != 4
             i += offset_i
-            result += fill(i, offset_j + 2)
+            result += fill(i, offset_j + 2, clear_contents=clear_middot)
             result += fill(i, offset_j + 3)
-            result += fill(i, offset_j + 4)
+            result += fill(i, offset_j + 4, clear_contents=clear_middot)
+        
         for i in range(offset_i, offset_i + 7):
             for j in range(offset_j, offset_j + 7):
                 result += no_border(i, j)
@@ -202,11 +214,13 @@ def generate_qr_template(qr_size, block_size=5, show_timing_marks=False):
 
     if qr_size > 21:
         # Fill the smaller inset registration box
-        html += fill(qr_size - 7, qr_size - 7)
+        html += fill(qr_size - 7, qr_size - 7, clear_contents=False)
 
+        # fill the sides
         for i in range(qr_size - 9, qr_size - 4):
-            html += fill(i, qr_size - 9)
-            html += fill(i, qr_size - 5)
+            clear_middot = i != (qr_size - 9) and i != (qr_size - 5)
+            html += fill(i, qr_size - 9, clear_contents=clear_middot)
+            html += fill(i, qr_size - 5, clear_contents=clear_middot)
         for j in range(qr_size - 8, qr_size - 5):
             html += fill(qr_size - 9, j)
             html += fill(qr_size - 5, j)
@@ -242,12 +256,12 @@ if __name__ == "__main__":
         description="""
     Generates a blank QR code template at 21x21, 25x25, or 29x29.
 
-        ex: python3 qr_code_template.py 25
-        ex: python3 qr_code_template.py 29
+        ex: python3 qr_code_template.py 25 12
+        ex: python3 qr_code_template.py 29 24
 
     Optionally change the block size divider guides:
 
-        python3 qr_code_template.py 29 --block_size 6
+        python3 qr_code_template.py 29 24 --block_size 6
         """,
         formatter_class=argparse.RawTextHelpFormatter
     )
@@ -257,6 +271,12 @@ if __name__ == "__main__":
                         type=int,
                         choices=[21, 25, 29],
                         help="QR code size. 21 for 21x21, 25 for 25x25, 29 for 29x29")
+    parser.add_argument('num_words',
+                        type=int,
+                        choices=[12, 24],
+                        help="Number of words in the mnemonic seed. 12 or 24")
+    
+    # Add optional arguments
     parser.add_argument('-b', '--block_size',
                         type=int,
                         default=5,
@@ -267,8 +287,8 @@ if __name__ == "__main__":
                         help="Show timing marks (the dashed blocks linking the three large registration boxes")
     args = parser.parse_args()
 
-    html = generate_qr_template(qr_size=args.qr_size, block_size=args.block_size, show_timing_marks=args.timing_marks)
-    file = open(f"qr_code_template_{args.qr_size}x{args.qr_size}.html", "w")
+    html = generate_qr_template(qr_size=args.qr_size, num_words=args.num_words, block_size=args.block_size, show_timing_marks=args.timing_marks)
+    file = open(f"qr_code_template_{args.num_words}words_{args.qr_size}x{args.qr_size}.html", "w")
     file.write(html)
     file.close()
 
