b576c048fca942a82eda94944152a5dff515b5ac
overcat <4catcode@gmail.com>
2023-06-05 23:07:47 +0800

    Add resources to package data

--- setup.py
+++ setup.py
@@ -23,4 +23,9 @@ setuptools.setup(
     package_dir={"": "src"},
     packages=setuptools.find_packages(where="src"),
     python_requires=">=3.6",
+    package_data={
+        'seedsigner': [
+            'resources/**',
+        ]
+    }
 )
