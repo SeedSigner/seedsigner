38fe9a25a1dcf50f1ba6725cf67a34e7e372a914
overcat <4catcode@gmail.com>
2023-06-05 23:03:52 +0800

    Add lumensigner resources to package data

--- setup.py
+++ setup.py
@@ -1,4 +1,5 @@
 import setuptools
+from setuptools import glob
 
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
@@ -23,4 +24,9 @@ setuptools.setup(
     package_dir={"": "src"},
     packages=setuptools.find_packages(where="src"),
     python_requires=">=3.7",
+    package_data={
+        'lumensigner': [
+            'resources/**',
+        ]
+    }
 )
