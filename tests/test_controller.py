6ad3f2acfd1a8d2d44ac3622ed5a986accdd4f70
overcat <4catcode@gmail.com>
2023-06-06 09:52:41 +0800

    Fix broken tests.

--- tests/test_controller.py
+++ tests/test_controller.py
@@ -1,6 +1,4 @@
-import configparser
 import pytest
-from mock import MagicMock
 from lumensigner.hardware.microsd import MicroSD
 from lumensigner.controller import Controller
 from lumensigner.models.settings_definition import SettingsConstants
@@ -32,14 +30,14 @@ def test_singleton_get_instance_preserves_state(reset_controller):
 
     # Initialize the instance and verify that it read the config settings
     controller = Controller.get_instance()
-    assert controller.unverified_address is None
+    assert controller.resume_main_flow is None
 
     # Change a value in the instance...
-    controller.unverified_address = "123abc"
+    controller.resume_main_flow = Controller.FLOW__SIGN_TX
 
     # ...get a new copy of the instance and confirm change
     controller = Controller.get_instance()
-    assert controller.unverified_address == "123abc"
+    assert controller.resume_main_flow == Controller.FLOW__SIGN_TX
 
 
 def test_missing_settings_get_defaults(reset_controller):
@@ -49,79 +47,39 @@ def test_missing_settings_get_defaults(reset_controller):
 
     # Settings defaults
     assert (
-        controller.settings.get_value(SettingsConstants.SETTING__LANGUAGE)
-        == SettingsConstants.LANGUAGE__ENGLISH
+            controller.settings.get_value(SettingsConstants.SETTING__LANGUAGE)
+            == SettingsConstants.LANGUAGE__ENGLISH
     )
     assert (
-        controller.settings.get_value(SettingsConstants.SETTING__WORDLIST_LANGUAGE)
-        == SettingsConstants.WORDLIST_LANGUAGE__ENGLISH
+            controller.settings.get_value(SettingsConstants.SETTING__WORDLIST_LANGUAGE)
+            == SettingsConstants.WORDLIST_LANGUAGE__ENGLISH
     )
     assert (
-        controller.settings.get_value(SettingsConstants.SETTING__PERSISTENT_SETTINGS)
-        == SettingsConstants.OPTION__DISABLED
-    )
-    assert controller.settings.get_value(SettingsConstants.SETTING__COORDINATORS) == [
-        i for i, j in SettingsConstants.ALL_COORDINATORS if i != "kpr"
-    ]
-    assert (
-        controller.settings.get_value(SettingsConstants.SETTING__BTC_DENOMINATION)
-        == SettingsConstants.BTC_DENOMINATION__THRESHOLD
+            controller.settings.get_value(SettingsConstants.SETTING__PERSISTENT_SETTINGS)
+            == SettingsConstants.OPTION__DISABLED
     )
 
-    # Advanced Settings defaults
     assert (
-        controller.settings.get_value(SettingsConstants.SETTING__NETWORK)
-        == SettingsConstants.MAINNET
+            controller.settings.get_value(SettingsConstants.SETTING__QR_DENSITY)
+            == SettingsConstants.DENSITY__MEDIUM
     )
     assert (
-        controller.settings.get_value(SettingsConstants.SETTING__QR_DENSITY)
-        == SettingsConstants.DENSITY__MEDIUM
+            controller.settings.get_value(SettingsConstants.SETTING__PASSPHRASE)
+            == SettingsConstants.OPTION__ENABLED
     )
     assert (
-        controller.settings.get_value(SettingsConstants.SETTING__XPUB_EXPORT)
-        == SettingsConstants.OPTION__ENABLED
-    )
-    assert controller.settings.get_value(SettingsConstants.SETTING__SIG_TYPES) == [
-        i for i, j in SettingsConstants.ALL_SIG_TYPES
-    ]
-    assert controller.settings.get_value(SettingsConstants.SETTING__SCRIPT_TYPES) == [
-        SettingsConstants.NATIVE_SEGWIT,
-        SettingsConstants.NESTED_SEGWIT,
-    ]
-    assert (
-        controller.settings.get_value(SettingsConstants.SETTING__XPUB_DETAILS)
-        == SettingsConstants.OPTION__ENABLED
+            controller.settings.get_value(SettingsConstants.SETTING__CAMERA_ROTATION)
+            == SettingsConstants.CAMERA_ROTATION__0
     )
     assert (
-        controller.settings.get_value(SettingsConstants.SETTING__PASSPHRASE)
-        == SettingsConstants.OPTION__ENABLED
+            controller.settings.get_value(SettingsConstants.SETTING__COMPACT_SEEDQR)
+            == SettingsConstants.OPTION__ENABLED
     )
     assert (
-        controller.settings.get_value(SettingsConstants.SETTING__CAMERA_ROTATION)
-        == SettingsConstants.CAMERA_ROTATION__0
+            controller.settings.get_value(SettingsConstants.SETTING__PRIVACY_WARNINGS)
+            == SettingsConstants.OPTION__ENABLED
     )
-    assert (
-        controller.settings.get_value(SettingsConstants.SETTING__COMPACT_SEEDQR)
-        == SettingsConstants.OPTION__ENABLED
-    )
-    assert (
-        controller.settings.get_value(SettingsConstants.SETTING__BIP85_CHILD_SEEDS)
-        == SettingsConstants.OPTION__DISABLED
-    )
-    assert (
-        controller.settings.get_value(SettingsConstants.SETTING__PRIVACY_WARNINGS)
-        == SettingsConstants.OPTION__ENABLED
-    )
-    assert (
-        controller.settings.get_value(SettingsConstants.SETTING__DIRE_WARNINGS)
-        == SettingsConstants.OPTION__ENABLED
-    )
-    assert (
-        controller.settings.get_value(SettingsConstants.SETTING__PARTNER_LOGOS)
-        == SettingsConstants.OPTION__ENABLED
-    )
-
     # Hidden Settings defaults
     assert (
-        controller.settings.get_value(SettingsConstants.SETTING__QR_BRIGHTNESS) == 189
+            controller.settings.get_value(SettingsConstants.SETTING__QR_BRIGHTNESS) == 189
     )
