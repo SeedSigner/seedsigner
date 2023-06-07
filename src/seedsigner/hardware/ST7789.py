6a2e02171314d7aec25fbbd27d7dd7fe840b2ab5
Jean Do <jean.dominique.lecomte.delisle@protonmail.me>
2023-06-07 08:31:37 -0400

    gBRG: G=most-significant, g=lesser not least

--- src/seedsigner/hardware/ST7789.py
+++ src/seedsigner/hardware/ST7789.py
@@ -153,7 +153,7 @@ class ST7789(object):
         if imwidth != self.width or imheight != self.height:
             raise ValueError('Image must be same dimensions as display \
                 ({0}x{1}).' .format(self.width, self.height))
-        # convert 24-bit RGB-8:8:8 to GBRg-3:5:5:3; then per-pixel byteswap to 16-bit RGB-5:6:5
+        # convert 24-bit RGB-8:8:8 to gBRG-3:5:5:3; then per-pixel byteswap to 16-bit RGB-5:6:5
         arr = array.array("H", Image.convert("BGR;16").tobytes())
         arr.byteswap()
         pix = arr.tobytes()
