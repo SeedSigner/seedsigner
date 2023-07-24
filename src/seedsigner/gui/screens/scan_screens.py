4a3afa8212b28a47e193f916f8ecdd004376ebd6
kdmukai <kdmukai@gmail.com>
2023-07-24 15:38:29 -0500

    Update scan_screens.py

--- src/seedsigner/gui/screens/scan_screens.py
+++ src/seedsigner/gui/screens/scan_screens.py
@@ -42,7 +42,7 @@ class ScanScreen(BaseScreen):
     decoder: DecodeQR = None
     instructions_text: str = "< back  |  Scan a QR code"
     resolution: tuple[int,int] = (480, 480)
-    framerate: int = 5  # TODO: alternate optimization for Pi Zero 2W
+    framerate: int = 4  # TODO: alternate optimization for Pi Zero 2W
     render_rect: tuple[int,int,int,int] = None
 
 
