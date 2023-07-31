56b6e238507c70892516979a42a91a4aed58d587
kdmukai <kdmukai@gmail.com>
2023-07-31 12:58:34 -0500

    docstring updates

--- tests/test_settings.py
+++ tests/test_settings.py
@@ -24,6 +24,8 @@ class TestSettings(BaseTest):
 
     def test_parse_settingsqr_data(self):
         """
+        SettingsQR parser should successfully parse a valid settingsqr input string and
+        return the resulting config_name and formatted settings_update_dict.
         """
         settings_name = "Test SettingsQR"
         settingsqr_data = f"""settings::v1 name={ settings_name.replace(" ", "_") } persistent=D coords=spa,spd denom=thr network=M qr_density=M xpub_export=E sigs=ss,ms scripts=nat,nes,tr xpub_details=E passphrase=E camera=180 compact_seedqr=E bip85=D priv_warn=E dire_warn=E partners=E"""
@@ -49,7 +51,7 @@ class TestSettings(BaseTest):
     
 
     def test_settingsqr_version(self):
-        """ Should accept SettingsQR v1 and reject any others """
+        """ SettingsQR parser should accept SettingsQR v1 and reject any others """
         settingsqr_data = "settings::v1 name=Foo"
         config_name, settings_update_dict = Settings.parse_settingsqr(settingsqr_data)
 
@@ -73,7 +75,7 @@ class TestSettings(BaseTest):
 
 
     def test_settingsqr_ignores_unrecognized_setting(self):
-        """ SettingsQR decoder should ignore unrecognized settings """
+        """ SettingsQR parser should ignore unrecognized settings """
         settingsqr_data = "settings::v1 name=Foo favorite_food=bacon xpub_export=D"
         config_name, settings_update_dict = Settings.parse_settingsqr(settingsqr_data)
 
@@ -85,7 +87,7 @@ class TestSettings(BaseTest):
 
 
     def test_settingsqr_fails_unrecognized_option(self):
-        """ SettingsQR decoder should fail if a settings has an unrecognized option """
+        """ SettingsQR parser should fail if a settings has an unrecognized option """
         settingsqr_data = "settings::v1 name=Foo xpub_export=Yep"
         with pytest.raises(InvalidSettingsQRData) as e:
             Settings.parse_settingsqr(settingsqr_data)
@@ -93,7 +95,7 @@ class TestSettings(BaseTest):
 
 
     def test_settingsqr_parses_line_break_separators(self):
-        """ SettingsQR decoder should read line breaks as acceptable separators """
+        """ SettingsQR parser should read line breaks as acceptable separators """
         settingsqr_data = "settings::v1\nname=Foo\nsigs=ss,ms\nscripts=nat,nes,tr\nxpub_export=E\n"
         config_name, settings_update_dict = Settings.parse_settingsqr(settingsqr_data)
 
