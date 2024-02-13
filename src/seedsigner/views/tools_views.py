c8eb81041edf5d6a6de0624d18db6e91e383453f
3rdIteration <steve@cryptoguide.tips>
2024-02-13 14:15:16 -0500

    dd bugfix & tweak

--- src/seedsigner/views/tools_views.py
+++ src/seedsigner/views/tools_views.py
@@ -1229,10 +1229,10 @@ class ToolsMicroSDFlashView(View):
             inNum = 1
             outNum = 0
             for errorLine in data_stderr_split:
-                if "Records In" in errorLine:
+                if "records in" in errorLine:
                     inNum = errorLine.split("+")[0]
                     continue
-                elif "Records Out" in errorLine:
+                elif "records out" in errorLine:
                     outNum = errorLine.split("+")[0]
                     continue
 
@@ -1310,9 +1310,9 @@ class ToolsMicroSDWipeZeroView(View):
         )
 
         if platform.uname()[1] == "seedsigner-os":
-            cmd = "dd if=/dev/zero of=/dev/mmcblk0 bs=1M count=1024"
+            cmd = "dd if=/dev/zero of=/dev/mmcblk0 bs=10M count=50"
         else:
-            cmd = "sudo dd if=/dev/zero of=/dev/mmcblk0 bs=1M count=1024"
+            cmd = "sudo dd if=/dev/zero of=/dev/mmcblk0 bs=10M count=50"
 
         data = run(cmd, capture_output=True, shell=True, text=True)
 
@@ -1323,10 +1323,10 @@ class ToolsMicroSDWipeZeroView(View):
         inNum = 1
         outNum = 0
         for errorLine in data_stderr_split:
-            if "Records In" in errorLine:
+            if "records in" in errorLine:
                 inNum = errorLine.split("+")[0]
                 continue
-            elif "Records Out" in errorLine:
+            elif "records out" in errorLine:
                 outNum = errorLine.split("+")[0]
                 continue
 
@@ -1366,9 +1366,9 @@ class ToolsMicroSDWipeRandomView(View):
         )
 
         if platform.uname()[1] == "seedsigner-os":
-            cmd = "dd if=/dev/urandom of=/dev/mmcblk0 bs=1M count=1024"
+            cmd = "dd if=/dev/urandom of=/dev/mmcblk0 bs=10M count=50"
         else:
-            cmd = "sudo dd if=/dev/urandom of=/dev/mmcblk0 bs=1M count=1024"
+            cmd = "sudo dd if=/dev/urandom of=/dev/mmcblk0 bs=10M count=50"
 
         data = run(cmd, capture_output=True, shell=True, text=True)
 
@@ -1379,10 +1379,11 @@ class ToolsMicroSDWipeRandomView(View):
         inNum = 1
         outNum = 0
         for errorLine in data_stderr_split:
-            if "Records In" in errorLine:
+            if "records in" in errorLine:
                 inNum = errorLine.split("+")[0]
                 continue
-            elif "Records Out" in errorLine:
+
+            if "records out" in errorLine:
                 outNum = errorLine.split("+")[0]
                 continue
 
