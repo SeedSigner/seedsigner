241e972cd4107d315dc97a96b4f0fe20135195cf
Daniel Bast <2790401+dbast@users.noreply.github.com>
2024-07-29 12:10:13 +0200

    More fixes for screen short generator

--- src/seedsigner/views/__init__.py
+++ src/seedsigner/views/__init__.py
@@ -1,3 +1,3 @@
-from .view import MainMenuView, BackStackView  # base class has to be first
+from .view import MainMenuView, BackStackView, NotYetImplementedView, PowerOptionsView, RestartView, UnhandledExceptionView  # base class has to be first
 
-__all__ = [MainMenuView, BackStackView]
+__all__ = [MainMenuView, BackStackView, NotYetImplementedView, PowerOptionsView, RestartView, UnhandledExceptionView]
