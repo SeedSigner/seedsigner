f3eeeeb9db740cd2c3f3be2150398f32582d3a04
kdmukai <kdmukai@gmail.com>
2024-08-07 12:13:34 -0500

    Typo, minor whitespace changes

--- src/seedsigner/views/psbt_views.py
+++ src/seedsigner/views/psbt_views.py
@@ -292,9 +292,10 @@ class PSBTAddressDetailsView(View):
             return Destination(PSBTFinalizeView)
 
 
+
 class PSBTChangeDetailsView(View):
     NEXT = "Next"
-    SKIP_VERIFICATION = "Skip Verificiation"
+    SKIP_VERIFICATION = "Skip Verification"
     VERIFY_MULTISIG = "Verify Multisig Change"
 
 
@@ -424,7 +425,6 @@ class PSBTChangeDetailsView(View):
             return Destination(BackStackView)
 
         elif button_data[selected_menu_num] == self.NEXT or button_data[selected_menu_num] == self.SKIP_VERIFICATION:
-
             if self.change_address_num < psbt_parser.num_change_outputs - 1:
                 return Destination(PSBTChangeDetailsView, view_args={"change_address_num": self.change_address_num + 1})
 
@@ -468,6 +468,7 @@ class PSBTAddressVerificationFailedView(View):
         # We're done with this PSBT. Route back to MainMenuView which always
         #   clears all ephemeral data (except in-memory seeds).
         return Destination(MainMenuView, clear_history=True)
+
 
 
 class PSBTOpReturnView(View):
