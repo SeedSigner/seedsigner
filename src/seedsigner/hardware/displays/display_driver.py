37a0a9d4c543f6a924b35c5015b653a88355d7a6
kdmukai <kdmukai@gmail.com>
2025-04-20 14:53:57 -0500

    bugfix after swapping st7789 dims

--- src/seedsigner/hardware/displays/display_driver.py
+++ src/seedsigner/hardware/displays/display_driver.py
@@ -12,19 +12,20 @@ class DisplayDriver:
         self.display_type = display_type
 
         if self.display_type == DISPLAY_TYPE__ST7789:
-            if height not in [240, 320] or width != 240:
+            if width not in [240, 320] or height != 240:
                 raise ValueError("ST7789 display only supports 240x240 or 320x240 resolutions")
 
-            if height == 240:
+            if width == 240:
                 # TODO: For now the original ST7789 driver has to be used for 240x240.
                 # The mpy version below renders incorrectly (almost like each row of pixels
                 # is one pixel short, so the entire screen exhibits a diagonal skew).
                 from seedsigner.hardware.displays.ST7789 import ST7789
                 self.display = ST7789()
 
-            elif height == 320:
+            elif width == 320:
                 from seedsigner.hardware.displays.st7789_mpy import ST7789
-                self.display = ST7789(width=width, height=height)
+                # Have to swap width and height; screen is natively 240x320
+                self.display = ST7789(width=height, height=width)
         
         elif self.display_type == DISPLAY_TYPE__ILI9341:
             from seedsigner.hardware.displays.ili9341 import ILI9341
