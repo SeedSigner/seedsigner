a18f46ad82c0f75811cf3304a8246d1e863e47b7
kdmukai <kdmukai@gmail.com>
2025-05-24 17:01:59 +0200

    Add screenshot for brightness tips during QR display

--- tests/screenshot_generator/generator.py
+++ tests/screenshot_generator/generator.py
@@ -1,3 +1,4 @@
+from dataclasses import dataclass
 import embit
 import pathlib
 import pytest
@@ -25,12 +26,14 @@ sys.modules['seedsigner.hardware.microsd'] = MagicMock()
 from seedsigner.controller import Controller
 from seedsigner.gui.components import GUIConstants
 from seedsigner.gui.renderer import Renderer
+from seedsigner.gui.screens.screen import BaseScreen
 from seedsigner.gui.screens.seed_screens import SeedAddPassphraseScreen
 from seedsigner.gui.toast import RemoveSDCardToastManagerThread, SDCardStateChangeToastManagerThread
 from seedsigner.gui.toast import DefaultToast, InfoToast, SuccessToast, WarningToast, ErrorToast, DireWarningToast
 from seedsigner.hardware.microsd import MicroSD
 from seedsigner.helpers import embit_utils
 from seedsigner.models.decode_qr import DecodeQR
+from seedsigner.models.encode_qr import BaseQrEncoder
 from seedsigner.models.psbt_parser import OPCODES, PSBTParser
 from seedsigner.models.qr_type import QRType
 from seedsigner.models.seed import Seed
@@ -39,7 +42,7 @@ from seedsigner.models.settings_definition import SettingsConstants, SettingsDef
 from seedsigner.views import (MainMenuView, PowerOptionsView, RestartView, NotYetImplementedView, UnhandledExceptionView, 
     psbt_views, seed_views, settings_views, tools_views, scan_views)
 from seedsigner.views.screensaver import OpeningSplashView
-from seedsigner.views.view import NetworkMismatchErrorView, OptionDisabledView, PowerOffView
+from seedsigner.views.view import NetworkMismatchErrorView, OptionDisabledView, PowerOffView, View
 
 from .utils import ScreenshotComplete, ScreenshotConfig, ScreenshotRenderer
 
@@ -260,6 +263,26 @@ def generate_screenshots(locale):
             controller.psbt_parser = PSBTParser(p=controller.psbt, seed=seed_12b)
 
 
+        # Mimic SeedExportXpubQRDisplayView calls in a light View + Screen so we can call
+        # `render_brightness_tip`.
+        class SeedExportXpubQR_ScreenBrightnessView(seed_views.SeedExportXpubQRDisplayView):
+            @dataclass
+            class QRDisplayScreenBrightnessTipScreen(BaseScreen):
+                qr_encoder: BaseQrEncoder = None
+
+                def _render(self):
+                    from seedsigner.gui.screens.screen import QRDisplayScreen
+                    image = self.qr_encoder.part_to_image(self.qr_encoder.cur_part(), 240, 240, border=2, background_color="white")
+                    QRDisplayScreen.QRDisplayThread.render_brightness_tip(None, image)
+                    self.renderer.show_image(image)
+
+            def run(self):
+                self.run_screen(
+                    SeedExportXpubQR_ScreenBrightnessView.QRDisplayScreenBrightnessTipScreen,
+                    qr_encoder=self.qr_encoder,  # initialized by SeedExportXpubQRDisplayView
+                )
+
+
         screenshot_sections = {
             "Main Menu Views": [
                 ScreenshotConfig(OpeningSplashView, dict(is_screenshot_renderer=True, force_partner_logos=True)),
@@ -300,7 +323,8 @@ def generate_screenshots(locale):
                 ScreenshotConfig(seed_views.SeedExportXpubCoordinatorView, dict(seed_num=0, sig_type="ss", script_type="nat")),
                 ScreenshotConfig(seed_views.SeedExportXpubWarningView, dict(seed_num=0, sig_type="msig", script_type="nes", coordinator="spd", custom_derivation="")),
                 ScreenshotConfig(seed_views.SeedExportXpubDetailsView, dict(seed_num=0, sig_type="ss", script_type="nat", coordinator="bw", custom_derivation="")),
-                #ScreenshotConfig(SeedExportXpubQRDisplayView),
+                ScreenshotConfig(SeedExportXpubQR_ScreenBrightnessView, dict(seed_num=0, coordinator="bw", derivation_path="m/84'/1'/0'")),
+
                 ScreenshotConfig(seed_views.SeedWordsWarningView, dict(seed_num=0)),
                 ScreenshotConfig(seed_views.SeedWordsView, dict(seed_num=0)),
                 ScreenshotConfig(seed_views.SeedWordsView, dict(seed_num=0, page_index=2), screenshot_name="SeedWordsView_2"),
