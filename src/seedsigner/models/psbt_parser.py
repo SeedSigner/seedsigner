21f2c2b848bb05a5ab16c6f875e0ccfe90ddbf1d
Ekrem BAL <mail.ekrembal@gmail.com>
2025-06-24 19:25:20 -0400

    Add taproot sigs

--- src/seedsigner/models/psbt_parser.py
+++ src/seedsigner/models/psbt_parser.py
@@ -234,6 +234,7 @@ class PSBTParser():
                 trimmed_psbt.inputs[i].final_scriptwitness = inp.final_scriptwitness
             else:
                 trimmed_psbt.inputs[i].partial_sigs = inp.partial_sigs
+                trimmed_psbt.inputs[i].taproot_sigs = inp.taproot_sigs
 
         return trimmed_psbt
 
@@ -247,6 +248,7 @@ class PSBTParser():
                 cnt += 1
             else:
                 cnt += len(list(inp.partial_sigs.keys()))
+                cnt += len(list(inp.taproot_sigs.keys()))
 
         return cnt
 
