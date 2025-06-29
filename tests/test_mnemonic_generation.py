16500a0c477d2d283c0d1d68c8fa0672cf89bbc5
Stargazer1010 <nitishitankar9102@gmail.com>
2025-06-29 23:51:31 +0530

    fix: test_mnemonic_generation.py

--- tests/test_mnemonic_generation.py
+++ tests/test_mnemonic_generation.py
@@ -223,9 +223,9 @@ def test_50_dice_rolls():
 
 def test_256_coin_flips():
     """ 256 coin flips input should yield the same 24-word mnemonic as iancoleman.io/bip39 """
-    # Check "Show entropy details", paste in coin flip sequence, click "Binary", select "Mnemonic Length" as "24 Words"
+    # Check "Show entropy details", paste in coin flip sequence, click "Binary", select "Mnemonic Length" as "Use Raw Entropy (3 words per 32 bits)"
     coin_flips = "1010101010101110110001000000001100100011000000000011000000001001110000000000000010000000000110000001110010110010000100110011001010101010101011101100010000000011001000110000000000110000000010011100000000000000100000000001100000011100101100100001001100110010"
-    expected = "grocery secret mountain turkey moment elbow size castle eagle water nominee general usual awesome attack daring cruise marriage debate tortoise document capital avocado engine"
+    expected = "primary involve absorb ecology adapt agent abandon avoid blossom tortoise luggage grab priority ginger arrive gas copy evoke ability ability limb flip error employ"
 
     mnemonic = mnemonic_generation.generate_mnemonic_from_coin_flips(coin_flips)
     actual = " ".join(mnemonic)
@@ -233,15 +233,15 @@ def test_256_coin_flips():
     assert actual == expected
 
     coin_flips = "0000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000"
-    expected = "gun library main saddle doctor meat pizza bone brave output matter chef merry flag abuse puppy first rotate era tent news arrest pepper finger"
+    expected = "abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon art"
 
     mnemonic = mnemonic_generation.generate_mnemonic_from_coin_flips(coin_flips)
     actual = " ".join(mnemonic)
     assert bip39.mnemonic_is_valid(actual)
     assert actual == expected
 
-    coin_flips = "1111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111"
-    expected = "rural oval civil ignore moon glide any pony perfect gain stable flag fortune require roast stereo mad guitar page flat reduce give borrow leisure"
+    coin_flips = "1111000101001010101010101010010000100111100000101010000101101101111000000101110101010010101010010101010001001011010101111101001001000010111110101011111101010010011010101001010111010100000011111001010010101101001010010101010010101010100101010010100101001001"
+    expected = "vanish fetch poverty excuse claw report lift prevent power pelican sting pig cook garden endless famous staff lake clip famous enjoy enhance pioneer click"
 
     mnemonic = mnemonic_generation.generate_mnemonic_from_coin_flips(coin_flips)
     actual = " ".join(mnemonic)
@@ -252,23 +252,23 @@ def test_256_coin_flips():
 
 def test_128_coin_flips():
     """ 128 coin flips input should yield the same 12-word mnemonic as iancoleman.io/bip39 """
-    # Check "Show entropy details", paste in coin flip sequence, click "Binary", select "Mnemonic Length" as "12 Words"
+    # Check "Show entropy details", paste in coin flip sequence, click "Binary", select "Mnemonic Length" as "Use Raw Entropy (3 words per 32 bits)"
     coin_flips = "10101010101011101100010000000011001000110000000000110000000010011100000000000000100000000001100000011100101100100001001100110010"
-    expected = "prevent style echo subway next museum palace lobster toward office shoe unfair"
+    expected = "primary involve absorb ecology adapt agent abandon avoid blossom tortoise luggage grape"
     mnemonic = mnemonic_generation.generate_mnemonic_from_coin_flips(coin_flips)
     actual = " ".join(mnemonic)
     assert bip39.mnemonic_is_valid(actual)
     assert actual == expected
 
     coin_flips = "00000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000"
-    expected = "earth naive tongue material rebel cotton credit quarter market peanut memory other"
+    expected = "abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon about"
     mnemonic = mnemonic_generation.generate_mnemonic_from_coin_flips(coin_flips)
     actual = " ".join(mnemonic)
     assert bip39.mnemonic_is_valid(actual)
     assert actual == expected
 
-    coin_flips = "11111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111"
-    expected = "exit pulp believe feature horror vehicle home more patrol hair drink resist"
+    coin_flips = "10100010100101010110101011010001010011101011111010010101010010101010101010010100101010101001010101010110101011111111000001001000"
+    expected = "penalty prize reform output true pipe prevent next next rely winter museum"
     mnemonic = mnemonic_generation.generate_mnemonic_from_coin_flips(coin_flips)
     actual = " ".join(mnemonic)
     assert bip39.mnemonic_is_valid(actual)
