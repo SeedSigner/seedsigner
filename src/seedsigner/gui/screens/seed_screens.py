1a120cf1f178cefd6620004a6fa9afcd688a9a1c
alvroble <50918598+alvroble@users.noreply.github.com>
2025-07-15 20:22:00 +0200

    Undo not needed change

--- src/seedsigner/gui/screens/seed_screens.py
+++ src/seedsigner/gui/screens/seed_screens.py
@@ -666,7 +666,6 @@ class SeedExportXpubDetailsScreen(WarningEdgesMixin, ButtonListScreen):
 @dataclass
 class SeedAddPassphraseScreen(BaseTopNavScreen):
     passphrase: str = ""
-    title = _("BIP-39 Passphrase")
 
     # Only used by the screenshot generator
     initial_keyboard: str = None
@@ -679,6 +678,7 @@ class SeedAddPassphraseScreen(BaseTopNavScreen):
 
 
     def __post_init__(self):
+        self.title = _("BIP-39 Passphrase")
         super().__post_init__()
 
         keys_lower = "abcdefghijklmnopqrstuvwxyz"
