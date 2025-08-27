c0b99da2193b811100b275ac13cd08baf881a313
notTanveer <tanveer.x.ansari@gmail.com>
2025-08-27 20:43:09 +0530

    refactor: use class based approach for seed type tracking

--- src/seedsigner/models/seed_storage.py
+++ src/seedsigner/models/seed_storage.py
@@ -9,7 +9,7 @@ class SeedStorage:
         self.seeds: List[Seed] = []
         self.pending_seed: Seed = None
         self._pending_mnemonic: List[str] = []
-        self._pending_is_electrum : bool = False
+        self._Seed_cls = Seed
 
 
     def set_pending_seed(self, seed: Seed):
@@ -35,15 +35,6 @@ class SeedStorage:
         self.pending_seed = None
 
 
-    def validate_mnemonic(self, mnemonic: List[str]) -> bool:
-        try:
-            Seed(mnemonic=mnemonic)
-        except InvalidSeedException as e:
-            return False
-        
-        return True
-
-
     def num_seeds(self):
         return len(self.seeds)
     
@@ -61,7 +52,7 @@ class SeedStorage:
 
     def init_pending_mnemonic(self, num_words:int = 12, is_electrum:bool = False):
         self._pending_mnemonic = [None] * num_words
-        self._pending_is_electrum = is_electrum
+        self._Seed_cls = ElectrumSeed if is_electrum else Seed
 
 
     def update_pending_mnemonic(self, word: str, index: int):
@@ -83,23 +74,17 @@ class SeedStorage:
 
     def get_pending_mnemonic_fingerprint(self, network: str = SettingsConstants.MAINNET) -> str:
         try:
-            if self._pending_is_electrum:
-                seed = ElectrumSeed(self._pending_mnemonic)
-            else:
-                seed = Seed(self._pending_mnemonic)
+            seed = self._Seed_cls(self._pending_mnemonic)
             return seed.get_fingerprint(network)
         except InvalidSeedException:
             return None
 
 
     def convert_pending_mnemonic_to_pending_seed(self):
-        if self._pending_is_electrum:
-            self.pending_seed = ElectrumSeed(self._pending_mnemonic)
-        else:
-            self.pending_seed = Seed(self._pending_mnemonic)
+        self.pending_seed = self._Seed_cls(self._pending_mnemonic)
         self.discard_pending_mnemonic()
     
 
     def discard_pending_mnemonic(self):
         self._pending_mnemonic = []
-        self._pending_is_electrum = False
+        self._Seed_cls = Seed
