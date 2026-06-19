b5ac223ca74e665a07fcc636a4fa7f8e17acab9f
rleed <rleed1@pm.me>
2026-06-19 06:04:33 +0000

    Update settings_definition.py

--- src/seedsigner/models/settings_definition.py
+++ src/seedsigner/models/settings_definition.py
@@ -643,7 +643,7 @@ class SettingsDefinition:
                       type=SettingsConstants.TYPE__SELECT_1,
                       visibility=SettingsConstants.VISIBILITY__ADVANCED,
                       selection_options=SettingsConstants.ALL_CAMERA_ROTATIONS,
-                      default_value=SettingsConstants.CAMERA_ROTATION__180),
+                      default_value=SettingsConstants.CAMERA_ROTATION__0),
 
         SettingsEntry(category=SettingsConstants.CATEGORY__FEATURES,
                       attr_name=SettingsConstants.SETTING__COMPACT_SEEDQR,
